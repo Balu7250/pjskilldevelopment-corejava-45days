21a3c00c1d6ad936e72663b5d484df640c8ccef6
PJSD <57825668+Balu7250@users.noreply.github.com>
2025-09-10 13:19:36 +0530

    Add DataTypesDemo.java with sample code for data types

---
+++ Day04_DataTypes/DataTypesDemo.java
@@ -0,0 +1,23 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        // Integer Data Type
+        int age = 25;
+        System.out.println("Age: " + age);
+
+        // Double Data Type
+        double salary = 55000.50;
+        System.out.println("Salary: " + salary);
+
+        // Character Data Type
+        char initial = 'A';
+        System.out.println("Initial: " + initial);
+
+        // Boolean Data Type
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+
+        // String Data Type
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+    }
+}
